d113e24078d65200e945321d6cdce88f50127f27
Frcsty <frostyprime13@gmail.com>
2022-10-11 12:31:57 +0200

    Update to 1.19.2, Add somewhat of an adventure support

--- src/main/java/io/samdev/actionutil/action/BroadcastAction.java
+++ src/main/java/io/samdev/actionutil/action/BroadcastAction.java
@@ -3,11 +3,12 @@ package io.samdev.actionutil.action;
 import me.clip.placeholderapi.libs.kyori.adventure.text.Component;
 import me.clip.placeholderapi.libs.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class BroadcastAction implements Action {
     public static void execute(Player player, Plugin plugin, Component message) {
-        Bukkit.broadcastMessage(LegacyComponentSerializer.legacyAmpersand().serialize(message));
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', LegacyComponentSerializer.legacyAmpersand().serialize(message)));
     }
 }
--- src/main/java/io/samdev/actionutil/action/MessageAction.java
+++ src/main/java/io/samdev/actionutil/action/MessageAction.java
@@ -2,11 +2,12 @@ package io.samdev.actionutil.action;
 
 import me.clip.placeholderapi.libs.kyori.adventure.text.Component;
 import me.clip.placeholderapi.libs.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class MessageAction implements Action {
     public static void execute(Player player, Plugin plugin, Component msg) {
-        player.sendMessage(LegacyComponentSerializer.legacyAmpersand().serialize(msg));
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', LegacyComponentSerializer.legacyAmpersand().serialize(msg)));
     }
 }
